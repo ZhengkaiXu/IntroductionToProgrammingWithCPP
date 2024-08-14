39c6dc30b33ea4e49c6c2728cdd250dcdb74bd81
ZhengkaiXu <695378091@qq.com>
2024-08-14 21:31:29 +0800

    welcome

---
+++ ch1/welcome.cpp
@@ -0,0 +1,12 @@
+//
+// Created by ZhengkaiXu on 2024/8/14.
+//
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Display Welcome to C++ to the console
+    cout << "Welcome to C++" << endl;
+
+    return 0;
+}
