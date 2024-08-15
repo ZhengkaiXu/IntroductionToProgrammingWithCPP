019833e0cd9fbf0d27adf486b402aac2fd5a0e48
ZhengkaiXu <695378091@qq.com>
2024-08-15 22:40:08 +0800

    WelcomeWithTreeMessages

---
+++ ch1/WelcomeWithTreeMessages.cpp
@@ -0,0 +1,12 @@
+//
+// Created by ZhengkaiXu on 2024/8/15.
+//
+#include <iostream>
+using namespace std;
+int main() {
+    cout << "Programming is fun!" << endl;
+    cout << "Fundamentals First" << endl;
+    cout << "Program Driven" << endl;
+
+    return 0;
+}
