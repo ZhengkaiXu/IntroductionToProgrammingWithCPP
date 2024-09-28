5fbad5348f6f99393287ad725e698bbddd031077
ZhengkaiXu <695378091@qq.com>
2024-09-28 22:31:50 +0800

    12-1

---
+++ ch11/CircleWithDestructor.h
@@ -0,0 +1,18 @@
+#ifndef CIRCLE_H
+#define CIRCLE_H
+
+class Circle {
+public:
+    Circle();
+    Circle(double);
+    ~Circle(); // Destructor
+    double getArea() const;
+    double getRadius() const;
+    void setRadius(double);
+    static int getNumebrOfObjects();
+private:
+    double radius;
+    static int numberOfObjects;
+};
+
+#endif
